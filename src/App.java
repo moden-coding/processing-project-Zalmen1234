4d31c8b5282ac024bf29ae7b4edf5a83d5dc73bd
Zalmen <zalmenzerach@gmail.com>
2024-03-07 08:32:34 -0500

    its working right now

--- src/App.java
+++ src/App.java
@@ -1,61 +1,147 @@
 import processing.core.*;
 
-
-
 public class App extends PApplet {
     public float diameter;
-    int x,y;
+    public float diameter2;
+    int x1, y1;
+    int x2, y2;
+
     boolean running = true;
-    public static void main(String[] args)  {
+    boolean firstRing = true;
+    public float ringCount = 0;
+
+    public static void main(String[] args) {
         PApplet.main("App");
     }
-    public void settings(){
-        size(800,600);
-    }
-    public void setup() {
-        background(0,255,255);
-        diameter = 0;
-        x=round(random(width));
-        y=round(random(height));
-        
-    }
-    public void draw(){
-        if(running=true){
-        background(0,255,255);
-        diameter+=2;
-        fill(255,0,0);
-        drawCircle(x,y,diameter);
-       
-        fill(0,255,255);
-        drawCircle(x,y,diameter-30);
-        if(diameter>1200) {
-            diameter = 0;
-            x=round(random(width));
-            y=round(random(height));
-        }
-       float distance = dist(x,y,width/2,height/2);
-        if(diameter>800 && distance>20){
-        running = false;    
-        }
-    } else {
-        background(0);
-    }
-        
-        
-    }
-    public void drawCircle(int x, int y, float diameter) {
-        ellipse(x,y,diameter,diameter);
-}
-public void keyPressed() {
 
-    if (keyCode == UP) {
-        y -= 10;
-    } else if (keyCode == DOWN) {
-        y += 10;
-    } else if (keyCode == RIGHT) {
-        x += 10;
-    } else if (keyCode == LEFT) {
-        x -= 10;
+    public void settings() {
+        size(800, 600);
     }
-}
+
+    public void setup() {
+        background(0, 255, 255);
+        diameter = 0;
+        diameter2 = 0;
+        x1 = round(random(width));
+        y1 = round(random(height));
+        x2 = round(random(width));
+        y2 = round(random(width));
+
+    }
+
+    public void draw() {
+        float distance = dist(x1, y1, width / 2, height / 2);
+        float distance2 = dist(x2, y2, width / 2, height / 2);
+        if (running == true) {
+            if (firstRing == true) {
+                background(0, 255, 255);
+                drawCloud(70, 70, 100);
+                drawCloud(400, 500, 200);
+                drawCloud(700, 200, 100);
+                if (ringCount > 0) {
+                    diameter2 += 2+ringCount;
+                    stroke(255, 0, 0);
+                    strokeWeight(20);
+                    fill(255, 0, 0, 0);
+                    drawCircle(x2, y2, diameter2);
+                }
+
+                diameter += 2+ringCount;
+
+                stroke(255, 0, 0);
+                strokeWeight(20);
+                fill(255, 0, 0, 0);
+                drawCircle(x1, y1, diameter);
+
+                if (diameter > 800) {
+                    drawCircle(x2, y2, diameter2);
+                    ringCount++;
+                    x2 = round(random(width));
+                    y2 = round(random(height));
+                    diameter2 = 0;
+                    firstRing = false;
+                }
+
+            } else {
+                background(0, 255, 255);
+                drawCloud(70, 70, 100);
+                drawCloud(400, 500, 200);
+                drawCloud(700, 200, 100);
+
+                stroke(255, 0, 0);
+                strokeWeight(20);
+                fill(255, 0, 0, 0);
+                drawCircle(x1, y1, diameter);
+                diameter += 2+ringCount;
+
+                diameter2 += 2+ringCount;
+                stroke(255, 0, 0);
+                strokeWeight(20);
+                fill(255, 0, 0, 0);
+                drawCircle(x2, y2, diameter2);
+                if (diameter2 > 800) {
+                    diameter = 0;
+                    x1 = round(random(width));
+                    y1 = round(random(width));
+                    firstRing = true;
+                    ringCount++;
+                }
+            }
+
+        } else {
+            background(0);
+
+        }
+
+        if (diameter > 800 && distance > 50) {
+            running = false;
+        }
+
+        if (diameter2 > 800 && distance2 > 50) {
+            running = false;
+        }
+
+    }
+
+    public void drawCircle(int x, int y, float diameter) {
+        ellipse(x, y, diameter, diameter);
+    }
+
+    public void drawCloud(int x, int y, float diameter) {
+        fill(255, 255, 255);
+        stroke(255, 255, 255);
+        ellipse(x, y, diameter, diameter);
+        ellipse(x + 50, y, diameter, diameter);
+        ellipse(x + 100, y, diameter, diameter);
+        ellipse(x + 80, y + 20, diameter, diameter);
+        ellipse(x + 30, y + 20, diameter, diameter);
+        ellipse(x + 20, y - 30, diameter, diameter);
+        ellipse(x + 80, y - 30, diameter, diameter);
+    }
+
+    public void keyPressed() {
+        if (firstRing == true) {
+            if (keyCode == UP) {
+                y1 -= 10;
+            } else if (keyCode == DOWN) {
+                y1 += 10;
+            } else if (keyCode == RIGHT) {
+                x1 += 10;
+            } else if (keyCode == LEFT) {
+                x1 -= 10;
+            }
+        }
+
+        if (firstRing == false) {
+            if (keyCode == UP) {
+                y2 -= 10;
+            } else if (keyCode == DOWN) {
+                y2 += 10;
+            } else if (keyCode == RIGHT) {
+                x2 += 10;
+            } else if (keyCode == LEFT) {
+                x2 -= 10;
+            }
+        }
+    }
 }
