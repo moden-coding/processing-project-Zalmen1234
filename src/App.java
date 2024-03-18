a613afd3c48f2af5225171ddee6924916ec28e7a
Zalmen <zalmenzerach@gmail.com>
2024-03-17 21:30:30 -0400

    DONE

--- src/App.java
+++ src/App.java
@@ -1,20 +1,26 @@
 import processing.core.*;
 
 public class App extends PApplet {
+//sets up variables for the diameters of the two rings
     public float diameter;
     public float diameter2;
+//sets up variables so the coordinates of the rings can be changed
     int x1, y1;
     int x2, y2;
-
+    
+//sets up variable to change the position of the birds' wings so they can flap
     int birdY = 0;
 
+//booleans to detect if the game is running, which ring is being controlled, 
+//and which direction the bird's wings are moving
     boolean running = true;
     boolean firstRing = true;
-    public float ringCount = 0;
     boolean goingUp = true;
-
+    public float ringCount = 0;
+//sets up coordinates of the clouds
     int cloudX = 0;
     int cloudY = 0;
+//booleans so that multiple keys can be pressed at once
     boolean upKey = false;
     boolean downKey = false;
     boolean rightKey = false;
@@ -32,6 +38,7 @@ public class App extends PApplet {
         background(0, 255, 255);
         diameter = 0;
         diameter2 = 0;
+//coordinates of the rings randomized
         x1 = round(random(width));
         y1 = round(random(height));
         x2 = round(random(width));
@@ -40,13 +47,17 @@ public class App extends PApplet {
     }
 
     public void draw() {
-        
+//sets up floats for the distance of the rings from the center.
      float distance = dist(x1, y1, width / 2, height / 2);
-        float distance2 = dist(x2, y2, width / 2, height / 2);
+     float distance2 = dist(x2, y2, width / 2, height / 2);
         
+//puts all the code under the running==true condition so that if you lose, the code stops
         if (running == true) {
-            
+//code is written twice - if the first ring is in play, the first ring is controlled,
+//if the second ring is in play, the second ring is controlled
             if (firstRing == true) {
+
+//sets up blue background and clouds
                 background(0, 255, 255);
                 drawCloud(70+cloudX, 70+cloudY, 100);
                 drawCloud(400+cloudX, 500+cloudY, 200);
@@ -54,11 +65,13 @@ public class App extends PApplet {
                 drawCloud(800+cloudX,700+cloudY, 100);
                 drawCloud(cloudX, 800+cloudY, 70);
                 drawCloud(-200+cloudX, 300+cloudY, 140);
-                
+
+//sets up a visible counter that displays how many rings have been passed through
                 fill(255,255,0);
                 textSize(48);
                 textAlign(CENTER,CENTER);
                 text("Rings: " + ringCount,700,500);
+//condition so that when the game starts, only the first ring is in play
                 if (ringCount > 0) {
                     diameter2 += 2+ringCount;
                     stroke(255, 0, 0);
@@ -67,12 +80,15 @@ public class App extends PApplet {
                     drawCircle(x2, y2, diameter2);
                 }
 
+//creates a ring that constantly increases in diameter as the code runs
                 diameter += 2+ringCount;
-
                 stroke(255, 0, 0);
                 strokeWeight(20);
                 fill(255, 0, 0, 0);
                 drawCircle(x1, y1, diameter);
+
+//when the first ring reaches a certain size, the second ring appears in a random position
+//the second ring is now being controlled as firstRing=false, and its diameter begins at 0
 
                 if (diameter > 800) {
                     drawCircle(x2, y2, diameter2);
@@ -82,8 +98,10 @@ public class App extends PApplet {
                     diameter2 = 0;
                     firstRing = false;
                 }
+//places the bird
                 bird();
 
+//when the arrow keys are pressed, the bird moves in that direction
                 if (upKey==true){
                     y1 += 5+ringCount;
                     cloudY += 5+ringCount;
@@ -100,7 +118,8 @@ public class App extends PApplet {
                     x1 += 5+ringCount;
                     cloudX += 5+ringCount;
                 }
-
+//code for when firstRing==false, meaning the second ring is in being controlled
+//nearly identical to code for first ring, just applying to the second ring
             } else {
                 background(0, 255, 255);
                 drawCloud(70+cloudX, 70+cloudY, 100);
@@ -117,17 +136,20 @@ public class App extends PApplet {
                 textAlign(CENTER,CENTER);
                 text("Rings: " + ringCount,700,500);
 
+//code so that the first ring still increases as before
                 stroke(255, 0, 0);
                 strokeWeight(20);
                 fill(255, 0, 0, 0);
                 drawCircle(x1, y1, diameter);
                 diameter += 2+ringCount;
-
+//code for the second ring being in play and increasing in size
                 diameter2 += 2+ringCount;
                 stroke(255, 0, 0);
                 strokeWeight(20);
                 fill(255, 0, 0, 0);
                 drawCircle(x2, y2, diameter2);
+//completes the cycle - if the second ring reaches 800, the first ring's diameter is reset
+// and firstRing becomes true again
                 if (diameter2 > 800) {
                     diameter = 0;
                     x1 = round(random(width));
@@ -154,7 +176,7 @@ public class App extends PApplet {
                     cloudX += 5+ringCount;
                 }
             }
-
+//code for if the code stops running, sets up the game over screen
         } else {
             background(0);
             fill(255,255,0);
@@ -164,7 +186,7 @@ public class App extends PApplet {
             
             textSize(48);
             textAlign(CENTER,CENTER);
-            text("Rings:"+ringCount, width/2, 500);
+            text("Rings:"+(ringCount-1), width/2, 500);
 
             fill(255,0,0);
             textSize(75);
@@ -173,7 +195,8 @@ public class App extends PApplet {
             
 
         }
-
+//if the diameter of either circle is too great and the circle is too far away from the center,
+//the game stops and it goes to the game over screen
         if (diameter > 800 && distance > 50) {
             running = false;
         }
@@ -185,10 +208,12 @@ public class App extends PApplet {
 
     }
 
+//method for drawing circles
     public void drawCircle(int x, int y, float diameter) {
         ellipse(x, y, diameter, diameter);
     }
 
+//method for drawing clouds by drawing many white circles
     public void drawCloud(int x, int y, float diameter) {
         fill(255, 255, 255);
         stroke(255, 255, 255);
@@ -201,6 +226,7 @@ public class App extends PApplet {
         ellipse(x + 80, y - 30, diameter, diameter);
     }
 
+//method for drawing the bird with the moving wings in the center of the screen
     public void bird() {
         fill(0,0,255);
         stroke(0);
@@ -228,7 +254,7 @@ public class App extends PApplet {
     }
 
     public void keyPressed() {
-        
+//if an arrow key is pressed, its boolean is activated so the bird moves in that direction until the key is released
             if (keyCode == UP) {
                 upKey = true;
             } if (keyCode == DOWN) {
@@ -238,7 +264,7 @@ public class App extends PApplet {
             } if (keyCode == LEFT) {
                 leftKey = true;
             }
-           
+//resets the game if r is pressed
             if ((keyCode=='r'||keyCode=='R')){
             
                 setup();
@@ -251,6 +277,7 @@ public class App extends PApplet {
             
         }
     }
+//code to stop the bird from moving in a direction once the arrow key is released
     public void keyReleased(){
         if (keyCode == UP){
             upKey = false;
