ead45dc586e813da09e1ba1db7116a324596f200
Zalmen <zalmenzerach@gmail.com>
2024-03-17 21:30:26 -0400

    FINAL

--- src/App.java
+++ src/App.java
@@ -6,9 +6,19 @@ public class App extends PApplet {
     int x1, y1;
     int x2, y2;
 
+    int birdY = 0;
+
     boolean running = true;
     boolean firstRing = true;
     public float ringCount = 0;
+    boolean goingUp = true;
+
+    int cloudX = 0;
+    int cloudY = 0;
+    boolean upKey = false;
+    boolean downKey = false;
+    boolean rightKey = false;
+    boolean leftKey = false;
 
     public static void main(String[] args) {
         PApplet.main("App");
@@ -30,14 +40,21 @@ public class App extends PApplet {
     }
 
     public void draw() {
-        float distance = dist(x1, y1, width / 2, height / 2);
+        
+     float distance = dist(x1, y1, width / 2, height / 2);
         float distance2 = dist(x2, y2, width / 2, height / 2);
+        
         if (running == true) {
+            
             if (firstRing == true) {
                 background(0, 255, 255);
-                drawCloud(70, 70, 100);
-                drawCloud(400, 500, 200);
-                drawCloud(700, 200, 100);
+                drawCloud(70+cloudX, 70+cloudY, 100);
+                drawCloud(400+cloudX, 500+cloudY, 200);
+                drawCloud(700+cloudX, 200+cloudY, 100);
+                drawCloud(800+cloudX,700+cloudY, 100);
+                drawCloud(cloudX, 800+cloudY, 70);
+                drawCloud(-200+cloudX, 300+cloudY, 140);
+                
                 fill(255,255,0);
                 textSize(48);
                 textAlign(CENTER,CENTER);
@@ -65,12 +82,34 @@ public class App extends PApplet {
                     diameter2 = 0;
                     firstRing = false;
                 }
+                bird();
+
+                if (upKey==true){
+                    y1 += 5+ringCount;
+                    cloudY += 5+ringCount;
+                }
+                if (downKey==true){
+                    y1 -= 5+ringCount;
+                    cloudY -= 5 + ringCount;
+                }
+                if (rightKey==true) {
+                    x1 -= 5+ringCount;
+                    cloudX -= 5+ringCount;
+                }
+                if (leftKey==true) {
+                    x1 += 5+ringCount;
+                    cloudX += 5+ringCount;
+                }
 
             } else {
                 background(0, 255, 255);
-                drawCloud(70, 70, 100);
-                drawCloud(400, 500, 200);
-                drawCloud(700, 200, 100);
+                drawCloud(70+cloudX, 70+cloudY, 100);
+                drawCloud(400+cloudX, 500+cloudY, 200);
+                drawCloud(700+cloudX, 200+cloudY, 100);
+                drawCloud(800+cloudX,700+cloudY, 100);
+                drawCloud(cloudX, 800+cloudY, 70);
+                drawCloud(-200+cloudX, 300+cloudY, 140);
+                
 
 
                 fill(255,255,0);
@@ -96,6 +135,24 @@ public class App extends PApplet {
                     firstRing = true;
                     ringCount++;
                 }
+                bird();
+
+                if (upKey==true){
+                    y2 += 5+ringCount;
+                    cloudY += 5+ringCount;
+                }
+                if (downKey==true){
+                    y2 -= 5+ringCount;
+                    cloudY -= 5 + ringCount;
+                }
+                if (rightKey==true) {
+                    x2 -= 5+ringCount;
+                    cloudX -= 5+ringCount;
+                }
+                if (leftKey==true) {
+                    x2 += 5+ringCount;
+                    cloudX += 5+ringCount;
+                }
             }
 
         } else {
@@ -103,11 +160,17 @@ public class App extends PApplet {
             fill(255,255,0);
             textSize(48);
             textAlign(CENTER,CENTER);
-            text("Press R to restart",width/2,500);
+            text("Press R to restart",width/2,300);
+            
+            textSize(48);
+            textAlign(CENTER,CENTER);
+            text("Rings:"+ringCount, width/2, 500);
+
             fill(255,0,0);
             textSize(75);
             textAlign(CENTER,CENTER);
-            text("YOU LOSE",width/2,300);
+            text("YOU LOSE",width/2,100);
+            
 
         }
 
@@ -118,6 +181,7 @@ public class App extends PApplet {
         if (diameter2 > 800 && distance2 > 50) {
             running = false;
         }
+    
 
     }
 
@@ -137,39 +201,68 @@ public class App extends PApplet {
         ellipse(x + 80, y - 30, diameter, diameter);
     }
 
+    public void bird() {
+        fill(0,0,255);
+        stroke(0);
+        strokeWeight(0);
+        circle(width/2,height/2,30);
+
+        stroke(0);
+        strokeWeight(0);
+        quad(415,315,415,285,465,285+birdY,465,315+birdY);
+        quad(385,315,385,285,335,285+birdY,335,315+birdY);
+        
+        if(goingUp==true){
+        birdY+=1;
+        } else {
+            birdY-=3;
+        }
+        if(birdY>10){
+            goingUp=false;
+        }
+        if(birdY<-10){
+            goingUp=true;
+        }        
+
+        
+    }
+
     public void keyPressed() {
-        if (firstRing == true) {
+        
             if (keyCode == UP) {
-                y1 -= 10+ringCount;
-            } else if (keyCode == DOWN) {
-                y1 += 10+ringCount;
-            } else if (keyCode == RIGHT) {
-                x1 += 10+ringCount;
-            } else if (keyCode == LEFT) {
-                x1 -= 10+ringCount;
+                upKey = true;
+            } if (keyCode == DOWN) {
+                downKey = true;
+            } if (keyCode == RIGHT) {
+                rightKey = true;
+            } if (keyCode == LEFT) {
+                leftKey = true;
             }
+           
+            if ((keyCode=='r'||keyCode=='R')){
             
-
+                setup();
+                cloudX=0;
+                cloudY=0;
+                running=true;
+                firstRing=true;
+                ringCount=0;
+            
+            
         }
-
-        if (firstRing == false) {
-            if (keyCode == UP) {
-                y2 -= 10+ringCount;
-            } else if (keyCode == DOWN) {
-                y2 += 10+ringCount;
-            } else if (keyCode == RIGHT) {
-                x2 += 10+ringCount;
-            } else if (keyCode == LEFT) {
-                x2 -= 10+ringCount;
-            }
+    }
+    public void keyReleased(){
+        if (keyCode == UP){
+            upKey = false;
         }
-        if ((keyCode=='r'||keyCode=='R')){
-            ringCount=0;
-            setup();
-            running=true;
-            
-            
-            
+        if (keyCode == DOWN){
+            downKey = false;
+        }
+        if (keyCode == RIGHT){
+            rightKey = false;
+        }
+        if (keyCode == LEFT) {
+            leftKey = false;
         }
     }
 }
